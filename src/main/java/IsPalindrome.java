aaf9a1cd9d3120e769b001768015e0de9f4eab08
busra idil <diyetisyenbusraidil@gmail.com>
2024-01-07 23:36:24 +0300

    V2

--- src/main/java/IsPalindrome.java
+++ src/main/java/IsPalindrome.java
@@ -3,12 +3,6 @@ import java.util.Scanner;
 public class IsPalindrome {
     public static void main(String[] args) {
 
-    /* Crteate a program checks if a String is PALINDROME or not.
-    If a word, phrase, or sequence that reads the same backword as forward then it is called "palindrome",
-    For Example : "madam" or "nursesrun" .
-    Kulanıcının girdiği bir String'in PALINDROME olup olmadığını kontrol eden bir method create ediniz
-    polindrome :tersten okunuşu da aynı olan ifadelerdir. Ornek : Ey edip Adanada pide ye, Traş niçin şart */
-
         Scanner scan = new Scanner(System.in);
         System.out.print("String bir ifade giriniz : ");
         String ifade = scan.nextLine();
